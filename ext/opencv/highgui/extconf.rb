a21a640a7eecb209bfb95415d0023428fb423bc2
oleg dashevskii <olegdashevskii@gmail.com>
2011-11-29 15:58:53 +0700

    More codecs

--- ext/opencv/highgui/extconf.rb
+++ ext/opencv/highgui/extconf.rb
@@ -15,6 +15,14 @@ if have_library('tiff')
   $defs.push '-DHAVE_TIFF'
 end
 
+if have_library('png')
+  $defs.push '-DHAVE_PNG'
+end
+
+if have_library('jasper')
+  $defs.push '-DHAVE_JASPER'
+end
+
 create_header('cvconfig.h')
 
 create_makefile("opencv_highgui")
